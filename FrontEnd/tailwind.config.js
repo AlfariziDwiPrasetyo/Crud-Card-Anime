4e38d3c11f833a25aba67a24ead010593abdac8a
AlfariziDwiPrasetyo <yoerdanatan@gmail.com>
2022-07-13 11:01:44 +0700

    font font tai

--- FrontEnd/tailwind.config.js
+++ FrontEnd/tailwind.config.js
@@ -4,8 +4,7 @@ module.exports = {
   theme: {
     extend: {
       fontFamily:{
-        helvetica:["Helvetica", "sans-serif"],
-        montserrat:["Montserrat", "sans-seric"]
+        montserrat:["Montserrat"]
       }
     },
   },
