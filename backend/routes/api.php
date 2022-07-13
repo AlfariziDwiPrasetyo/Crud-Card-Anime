4526b02d9bc3d917cdfcbc8430fe8ee942b84f3d
ashafaatadhis <skullreaper387@gmail.com>
2022-07-13 11:17:59 +0700

    Menambahkan Waifu controller, waifu models, dan route resource GET method

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\WaifuController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -13,6 +14,12 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+
+// Route::get('/cuy', function(Request $request){
+//     return response()->json(['msg' => 'oke saja']);
+// });
+
+Route::resource('/waifu', WaifuController::class);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
