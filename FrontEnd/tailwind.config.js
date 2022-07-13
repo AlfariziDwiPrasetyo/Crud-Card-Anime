414deb0c03e6497eb604f6188d349654f62ee660
ashafaatadhis <skullreaper387@gmail.com>
2022-07-13 11:48:59 +0700

    Memperbaiki bug font

--- FrontEnd/tailwind.config.js
+++ FrontEnd/tailwind.config.js
@@ -1,12 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./public/**/*{html,js}'],
+  content: ["./public/**/*{html,js}"],
   theme: {
     extend: {
-      fontFamily:{
-        montserrat:["Montserrat"]
-      }
+      fontFamily: {
+        montserrat: ["Montserrat"],
+        helvetica: ["Helvetica"],
+      },
     },
   },
   plugins: [],
-}
+};
