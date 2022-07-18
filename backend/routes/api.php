ebd5b3b1d24c9cf2505f3963ed00b7be20f820ee
AlfariziDwiPrasetyo <yoerdanatan@gmail.com>
2022-07-18 10:37:53 +0700

    Solved

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,8 +1,9 @@
 <?php
 
+use App\Http\Controllers\WaifuController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\WaifuController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -14,7 +15,6 @@ use App\Http\Controllers\WaifuController;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-
 Route::resource('/waifu', WaifuController::class);
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
